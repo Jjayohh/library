90936384f987bec8c8ccedda370cd10d7b316e53
Jordan F <jordanfracassi@icloud.com>
2023-04-30 23:48:26 +0000

    commit 4

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-// class Book {
-//   constructor(title, author, pages, read) {
-//     this.title = form.title.value;
-//     this.author = form.author.value;
-//     this.pages = form.pages.value + "pages";
-//     this.read = form.read.checked;
-//   }
-// }
-
-// let myLibrary = [];
-// //console.log(myLibrary);
-// let newBook;
-
-function addBookToLibrary() {
-  newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  SVGMetadataElement();
-  WebGL2RenderingContext();
-  form.reset();
-}
-
-const popup = document.getElementById("popup");
-const libraryBoxes = document.getElementById("libraryBoxes");
-// const bookTitle = document.getElementById("#title");
-// const bookAuthor = document.getElementById("#author");
-// const bookPages = document.getElementById("#pages");
-// const bookRead = document.getElementById("#read");
 const addBtn = document.querySelector("#subBtn");
-const DivContainer = document.getElementById("libraryBoxes");
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-// });
+const popup = document.getElementById("popup");
 
 function openPopup() {
   popup.classList.add("open-popup");
@@ -40,11 +9,78 @@ function closePopup() {
   popup.classList.remove("open-popup");
 }
 
-function addBook() {
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = form.title.value;
+    this.author = form.author.value;
+    this.pages = form.pages.value + "pages";
+    this.read = form.read.checked;
+  }
+}
+
+let myLibrary = [];
+let newBook;
+
+function addBookToLibrary() {
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  SVGMetadataElement();
+  WebGL2RenderingContext();
+  render();
+  form.reset();
+}
+
+function render() {
+  const display = document.getElementById("libraryBoxes");
+  const books = document.querySelectorAll(".book");
+  books.forEach((book) => display.removeChild(book));
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBook(myLibrary[i]);
+  }
+}
+
+function addBook(item) {
   const newDiv = document.createElement("div");
+  const DivContainer = document.getElementById("libraryBoxes");
+  const bookTitle = document.getElementById("title");
+  const bookAuthor = document.getElementById("author");
+  const bookPages = document.getElementById("pages");
+  // const bookRead = document.getElementById("read");
 
   newDiv.classList.add("book");
+  newDiv.setAttribute("id", myLibrary.indexOf(item));
+
+  bookTitle.classList.add("title");
+  bookTitle.textContent = item.title;
+  newDiv.appendChild(bookTitle);
+
+  bookAuthor.classList.add("author");
+  bookAuthor.textContent = item.author;
+  newDiv.appendChild(bookAuthor);
+
+  bookPages.classList.add("pages");
+  bookPages.textContent = item.pages;
+  newDiv.appendChild(bookPages);
+
   DivContainer.appendChild(newDiv);
 }
 
-addBtn.addEventListener("click", addBook);
+addBtn.addEventListener("click", addBook, render);
+
+function setData() {
+  localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
+
+function restore() {
+  if (!localStorage.myLibrary) {
+    render();
+  } else {
+    let objects = localStorage.getItem("myLibrary"); // gets information from local storage to use in below loop to create DOM/display
+    objects = JSON.parse(objects);
+    myLibrary = objects;
+    render();
+  }
+}
+
+restore();
